669db821d765b31118a70a70df0165fff30d15c9
SamuelHSA <longimusplay@gmail.com>
2022-12-12 18:59:15 -0300

    Inicio da integração JWT
    Aula de JWT do curso da Devaria

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
 import type { respostaPadraoMsg } from '../../types/respostaPadraoMsg';
 import type { cadastroRequisicao } from '../../types/cadastroRequisicao';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
+
+
+import md5 from 'md5';
 
 const endpointCadastro = 
     async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
@@ -23,11 +27,22 @@ const endpointCadastro =
             return res.status(400).json({erro : 'Senha inválida'});
         }
 
+        // Validação se já existe usuário com o mesmo email
+        const usuarioComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0){
+            return res.status(400).json({erro : 'Já existe uma conta com o email informado'}); 
+        }
+
         // Salvar no Banco de dados
-        await UsuarioModel.create(usuario);
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : usuario.senha
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg : 'Usuario criado com sucesso'});
     }
     return res.status(405).json({erro: 'Método informado não é válido'});
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type { respostaPadraoMsg } from '../../types/respostaPadraoMsg';
+import type { loginResposta } from '../../types/loginResposta';
+import md5 from 'md5';
+import { UsuarioModel } from "../../models/UsuarioModel";
+import jwt from 'jsonwebtoken';
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
-    res : NextApiResponse<respostaPadraoMsg>
+    res : NextApiResponse<respostaPadraoMsg | loginResposta>
 ) => {
+
+    const {MINHA_CHAVE_JWT} = process.env;
+    if(!MINHA_CHAVE_JWT){
+        return res.status(500).json({erro : 'ENV jwt não informada'});
+    }
+
     if(req.method === 'POST') {
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' &&
-            senha === 'admin@123'){
-                return res.status(200).json({msg : 'Usuário autenticado com sucesso'});
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : senha});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0];
+
+            const token = jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            return res.status(200).json({
+                nome : usuarioEncontrado.nome, 
+                email : usuarioEncontrado.email, 
+                token});
         }
         return res.status(405).json({erro: 'Usuário ou senha não encontrado'});
     }
