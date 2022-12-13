c44ca132be66069745cb1118d0ad34ae5e94329e
SamuelHSA <longimusplay@gmail.com>
2022-12-13 20:19:03 -0300

    Onde salvaremos nossas imagens
    Deu erro, por isso vou resolver no outro dia

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -3,16 +3,16 @@ import type { respostaPadraoMsg } from '../../types/respostaPadraoMsg';
 import type { cadastroRequisicao } from '../../types/cadastroRequisicao';
 import { UsuarioModel } from '../../models/UsuarioModel';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
-
-
 import md5 from 'md5';
+import { upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
+import nc from 'next-connect';
 
-const endpointCadastro = 
-    async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
-
-    if(req.method === 'POST') {
-        const usuario = req.body as cadastroRequisicao;
-
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
+        try{
+            const usuario = req.body as cadastroRequisicao;
+                console.log('usuario', usuario)
         if(!usuario.nome || usuario.nome.length < 2) {
             return res.status(400).json({erro : 'Nome inválido'});
         }
@@ -26,23 +26,37 @@ const endpointCadastro =
         if(!usuario.senha || usuario.senha.length < 4) {
             return res.status(400).json({erro : 'Senha inválida'});
         }
-
+    
         // Validação se já existe usuário com o mesmo email
         const usuarioComMesmoEmail = await UsuarioModel.find({email : usuario.email});
         if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0){
             return res.status(400).json({erro : 'Já existe uma conta com o email informado'}); 
         }
 
+        // Enviar a imagem do multer para o cosmic
+        const image = await uploadImagemCosmic(req);
+
         // Salvar no Banco de dados
         const usuarioASerSalvo = {
             nome : usuario.nome,
             email : usuario.email,
-            senha : usuario.senha
+            senha : md5(usuario.senha),
+            avatar : image?.media?.url
         }
-        await UsuarioModel.create(usuarioASerSalvo);
-        return res.status(200).json({msg : 'Usuario criado com sucesso'});
+            await UsuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({msg : 'Usuario criado com sucesso'});
+        }catch(e){
+            console.log(e);
+            return res.status(500).json({erro : 'Erro ao cadastrar usuario'});
+
+        }
+});
+    
+
+export const config = {
+    api: {
+        bodyParser : false
     }
-    return res.status(405).json({erro: 'Método informado não é válido'});
 }
 
-export default conectarMongoDB(endpointCadastro);
+export default conectarMongoDB(handler)
