6353ac1364ebcb01db5bc45a778f3734a15edd17
SamuelHSA <longimusplay@gmail.com>
2022-12-15 20:06:42 -0300

    Get do usuário e feed pelo id do user

---
+++ pages/api/feed.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { respostaPadraoMsg } from '../../types/respostaPadraoMsg'
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT'
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
+import { UsuarioModel } from '../../models/UsuarioModel';
+import { publicacaoModel } from '../../models/publicacaoModel';
+
+const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg | any>) => {
+    try{
+        if(req.method === 'GET') {
+            if(req?.query?.id) {
+                // Agora que tenho o id do usuário
+                // Como eu válido se o usuário é válido
+                const usuario = await UsuarioModel.findById(req?.query?.id);
+                if(!usuario) {
+                    return res.status(405).json({erro : 'Usuário não encontrado'});
+                }
+
+                // E como eu busco as publicações dele?
+                const publicacoes = await publicacaoModel
+                    .find({idUsuario : usuario.id})
+                    .sort({data : -1});
+                    
+                    return res.status(200).json(publicacoes);
+            }
+        }
+        return res.status(405).json({erro : 'Método informado não é válido'});
+    }catch(e){
+        console.log(e);
+    }
+        return res.status(400).json({erro : 'Não foi possível obter o feed'});
+}
+
+export default validarTokenJWT(conectarMongoDB(feedEndpoint));
